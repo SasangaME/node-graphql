3e3a199dcc1fd0f3bf34fdac0e39b0c00f84f741
SasangaME <madhumaledirisinghe@gmail.com>
2022-10-19 09:52:41 +0530

    graphql endpoint without db

---
+++ README.md
@@ -0,0 +1,9 @@
+# NodeJS Express Graphql Project
+
+## Type System
+type Warrior {
+  id: ID!
+  name: String!
+}
+
+`!` means non-nullable
--- index.js
+++ index.js
@@ -1,8 +1,41 @@
 import express from 'express';
 import cors from 'cors';
 import { config } from 'dotenv';
+import { graphqlHTTP } from 'express-graphql';
+import { makeExecutableSchema } from '@graphql-tools/schema'
 
 config();
+
+const data = {
+    warriors: [
+        { id: '001', name: 'Jaime' },
+        { id: '002', name: 'Jorah' },
+    ],
+};
+
+// Schema
+const typeDefs = `
+type Warrior {
+    id: ID!
+    name: String!
+}
+
+type Query {
+    warriors: [Warrior]
+}
+`;
+
+// Resolver for warriors
+const resolvers = {
+    Query: {
+        warriors: (obj, args, context) => context.warriors,
+    },
+};
+
+const executableSchema = makeExecutableSchema({
+    typeDefs,
+    resolvers,
+});
 
 const app = express();
 
@@ -18,6 +51,17 @@ app.use(express.urlencoded({
 app.get('/', (req, res) => {
     res.json({ message: "server is running" });
 });
+
+// Entrypoint
+app.use(
+    '/graphql',
+    graphqlHTTP({
+        schema: executableSchema,
+        context: data,
+        graphiql: true,
+    })
+)
+
 
 
 const port = process.env.PORT || 3000;
